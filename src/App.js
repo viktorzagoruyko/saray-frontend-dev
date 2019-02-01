77165709f4109d522bd7864f5809e44aa61a9745
Viktor Zagoruyko <viktorzagoruyko@users.noreply.github.com>
2019-02-02 01:53:20 +0500

    Hanging around

--- src/App.js
+++ src/App.js
@@ -7,15 +7,9 @@ import Home from './containers/Home/index.jsx';
 export default class App extends React.Component {
     render() {
         return (
-            <div>
-                <h1>React Application</h1>
-                <p>Author: Viktor Zagoruyko</p>
-                <ul>
-                    <li>
-                        <Home />
-                    </li>
-                </ul>
-            </div>
+            <section>
+                <Home />
+            </section>
         );
     }
 }
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
+const container = styled.div`
+    width: 100%;
+    height: 100%;
+    
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    
+    & h1 {
+        font-size: 3rem;
+        font-weight: 700;
+    }
+    
+    & h6 {
+        font-size: 1rem;
+        font-weight: 300;
+    }
+`;
+
 export default class Home extends React.Component {
     render() {
         return (
-            <h3>Test of .jsx extension</h3>
+            <div>
+                <h1>SARAY</h1>
+                <h6>PHOTOSTUDIO</h6>
+            </div>
         )
     }
 }
