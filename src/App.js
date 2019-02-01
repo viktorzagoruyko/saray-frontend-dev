0ddc4c8f3d15afda90b374a5939f3c0c5f8c248c
Viktor Zagoruyko <viktorzagoruyko@users.noreply.github.com>
2019-02-01 18:24:37 +0500

    Test #2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <div>
+            <h6>Hlodvig the Dog</h6>
             <h1>Hello world!</h1>
             <h6>It's me, Mario!</h6>
         </div>
