2a85173db6c7aed98605f8760d729ac840fdeb7b
Viktor Zagoruyko <viktorzagoruyko@users.noreply.github.com>
2019-02-02 18:42:26 +0500

    Home page

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -2,33 +2,84 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-const Container = styled.div`
-    width: 100%;
-    height: 100vh;
+const Video = styled.video`
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+    margin: auto;
+    width: 650px;
+    max-width: 100vw;
+`;
+
+const Container = styled.section`
+    position: relative;
+    overflow: hidden;
     
+    width: 100%;
+`;
+
+class ResizableSection extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            height: '',
+        };
+
+        this.handleWindowResize = this.handleWindowResize.bind(this);
+    }
+
+    handleWindowResize() {
+        this.setState({
+            height: window.innerHeight + 'px'
+        });
+    }
+
+    componentWillMount() {
+        this.handleWindowResize();
+    }
+    
+    componentDidMount() {
+        window.addEventListener('resize', this.handleWindowResize);
+    }
+    
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowResize);
+    }
+
+    render() {
+        return <Container style={{height: this.state.height}} {...this.props}>{this.props.children}</Container>
+    }
+}
+
+const Section = styled(ResizableSection)`
     display: flex;
-    flex-direction: column;
     justify-content: center;
     align-items: center;
-    
-    & h1 {
-        font-size: 3rem;
-        font-weight: 700;
-    }
-    
-    & h6 {
-        font-size: 1.35rem;
-        font-weight: 300;
-    }
 `;
 
 export default class Home extends React.Component {
     render() {
         return (
-            <Container>
-                <h1>SARAY</h1>
-                <h6>PHOTOSTUDIO</h6>
-            </Container>
+            <Section>
+                <Video loop autoPlay playsInline muted>
+                    <source src="http://138.197.142.45:8000/static/background.webm" />
+                    <source src="http://138.197.142.45:8000/static/background.mp4" />
+                </Video>
+                <svg width={'100%'} height={'100%'} viewBox={'0 0 325 140'} preserveAspectRatio="xMidYMid slice">
+                    <defs>
+                        <mask id={'overlay'}>
+                            <rect width={'100%'} height={'100%'} fill={Theme.white} />
+                            <text x={'50%'} y={'50%'} fontSize={'20px'} fontWeight={'700'} fontFamily={'Roboto, sans-serif'} textAnchor={'middle'}>SARAY</text>
+                            <text x={'50%'} y={'56%'} fontSize={'9px'} fontWeight={'200'} fontFamily={'Roboto, sans-serif'} textAnchor={'middle'}>PHOTO STUDIO</text>
+                        </mask>
+                    </defs>
+                    <rect width={'100%'} height={'100%'} fill={Theme.white} mask={'url(#overlay)'} />
+                </svg>
+            </Section>
         )
     }
 }
