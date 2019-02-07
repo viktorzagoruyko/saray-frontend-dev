30bfe860f3525dd60e11093c56162e0ac6a391ac
Viktor Zagoruyko <vik.zgrk@gmail.com>
2019-02-07 21:25:10 +0500

    New HomePage design

--- src/containers/HomeContainer/index.jsx
+++ src/containers/HomeContainer/index.jsx
@@ -1,38 +1,25 @@
 import React from 'react';
 
-import { SectionMobile, VideoMobile, SectionDesktop, VideoDesktop, Logo, Text } from './style';
+import { Section, Video, Logo } from './style';
 
 export default function HomeContainer() {
     return(
-        <>
-            <SectionMobile>
-                <VideoMobile loop autoPlay playsInline muted>
+        <div className='horizontalPage'>
+            <Section className='horizontalSection'>
+                <Video loop autoPlay playsInline muted>
                     <source src="http://138.197.142.45:8000/static/background.webm" />
                     <source src="http://138.197.142.45:8000/static/background.mp4" />
-                </VideoMobile>
+                </Video>
 
-                <svg width={'100%'} height={'100%'} viewBox={'0 0 325 140'} preserveAspectRatio="xMidYMid slice">
-                    <defs>
-                        <mask id={'overlay'}>
-                            <rect width={'100%'} height={'100%'} fill={'#FDFDFD'} />
-                            <text x={'50%'} y={'50%'} fontSize={'20px'} fontWeight={'700'} fontFamily={'Roboto, sans-serif'} textAnchor={'middle'}>SARAY</text>
-                            <text x={'50%'} y={'56%'} fontSize={'9px'} fontWeight={'200'} fontFamily={'Roboto, sans-serif'} textAnchor={'middle'}>PHOTO STUDIO</text>
-                        </mask>
-                    </defs>
-                    <rect width={'100%'} height={'100%'} fill={'#FDFDFD'} mask={'url(#overlay)'} />
-                </svg>
-            </SectionMobile>
-            <SectionDesktop>
-                <VideoDesktop loop autoPlay playsInline muted>
-                    <source src="http://138.197.142.45:8000/static/background.webm" />
-                    <source src="http://138.197.142.45:8000/static/background.mp4" />
-                </VideoDesktop>
                 <Logo>
-                    <Text>
-                        <span>SARAY</span><br /><span>PHOTOSTUDIO</span>
-                    </Text>
+                    <span>Saray</span>
+                    <br />
+                    <span>PhotoStudio</span>
                 </Logo>
-            </SectionDesktop>
-        </>
+            </Section>
+            <div className='horizontalSection'>
+            
+            </div>
+        </div>
     )
 }
--- src/containers/HomeContainer/style.js
+++ src/containers/HomeContainer/style.js
@@ -2,80 +2,73 @@ import styled from 'styled-components';
 
 import ResizableSection, { media } from '../../style/styled';
 
-export const SectionMobile = styled(ResizableSection)`
-    display: flex;
-    justify-content: center;
-    align-items: center;
+export const Section = styled(ResizableSection)`
+    position: relative;
 
+    padding-left: 30px;
+    padding-right: 30px;
+
+    display: flex;
+    align-items: center;
+    
     ${ media.landscape`
-        display: none;
+        padding-left: 50px;
+        padding-right: 50px;
+    `}
+
+    ${ media.desktop`
+        padding-left: 75px;
+        padding-right: 75px;
     `}
 `;
 
-export const VideoMobile = styled.video`
+export const Video = styled.video`
     position: absolute;
     left: 0;
     top: 0;
     right: 0;
     bottom: 0;
     z-index: -1;
+
     margin: auto;
-    width: 650px;
-    max-width: 100vw;
-
-    ${ media.landscape`
-        display: none;
-    `}
+    height: 100%;
+    width: auto;
 `;
 
-export const SectionDesktop = styled.section`
+export const Logo = styled.h1`
+    text-transform: uppercase;
+
+    & span {
+        color: #FDFDFD;
+    }
+
+    & span:nth-child(1) {
+        font-size: 3rem;
+        font-weight: 700;
+    }
+
+    & span:nth-child(3) {
+        font-size: 2.5rem;
+        font-weight: 200;
+    }
+
     ${ media.landscape`
-        position: relative;
-
-        padding-left: 50px;
-        padding-right: 50px;
-        height: 100vh;
-        background-color: #FDFDFD;
-
-        display: flex;
-        
-        align-items: center;
-    `}
-`;
-
-export const VideoDesktop = styled.video`
-    ${ media.landscape`
-        position: absolute;
-        top: 0;
-        left: 0;
-
-        width: auto;
-        height: 100%;
-    `}
-`;
-
-export const Logo = styled.div`
-    ${ media.landscape`
-        z-index: 1;
-    `}
-`;
-
-export const Text = styled.h1`
-    ${ media.landscape`
-        & span {
-            color: #FDFDFD;
-            
-            opacity: 1;
-        }
-
         & span:nth-child(1) {
-            font-size: 6rem;
-            font-weight: 700;
+           font-size: 6rem;
         }
 
         & span:nth-child(3) {
-            font-size: 4rem;
-            font-weight: 200;
+           font-size: 4rem;
+        }
+    `}
+
+    ${ media.desktop`
+        & span:nth-child(1) {
+           font-size: 7rem;
+        }
+
+        & span:nth-child(3) {
+           font-size: 5rem;
         }
     `}
 `;
