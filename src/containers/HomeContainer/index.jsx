546276cecb168380a1d4afc3d822e6bfdeacc7ea
Viktor Zagoruyko <vik.zgrk@gmail.com>
2019-02-08 04:13:30 +0500

    New background video for Home page

--- src/containers/HomeContainer/index.jsx
+++ src/containers/HomeContainer/index.jsx
@@ -6,8 +6,8 @@ export default function HomeContainer() {
     return(
         <Section>
             <Video loop autoPlay playsInline muted>
-                <source src="http://138.197.142.45:8000/static/background.webm" />
-                <source src="http://138.197.142.45:8000/static/background.mp4" />
+                <source src="https://drive.google.com/uc?export=download&id=1cEWVcpz-mKXKHdXVZ02meVaGPr1_2OCm" type='video/webm' />
+                <source src="https://drive.google.com/uc?export=download&id=1M2EJfkdVicu72qww8SbJTx1d82PrdLJG" type='video/mp4' />
             </Video>
 
             <Logo>
